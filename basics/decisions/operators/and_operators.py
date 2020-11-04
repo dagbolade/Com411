6408f0e781af2b5a380cd50ef2eebf1b4c9cd5c1
repl.it user <replituser@example.com>
2020-11-04 18:40:55 +0000

    added code to show logical AND operation

---
+++ basics/decisions/operators/and_operators.py
@@ -0,0 +1,10 @@
+print("what did i hear")
+user = input().lower()
+
+print("what did i see")
+user_see = input().lower()
+
+if (user == "grrr") and (user_see == "two red eyes"):
+  print("There is a scary creature. I should get out of here!")
+else:
+  print("I am a little scared but I will continue.")  
