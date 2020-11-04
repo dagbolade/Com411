5ac73f0d3d1a7b1a42cbc50d25bf05931856be3e
repl.it user <replituser@example.com>
2020-11-04 15:15:03 +0000

    Added code to display the use of a modulo

---
+++ basics/decisions/simple_decisions/modulo_operator.py
@@ -0,0 +1,9 @@
+#using a modulor
+print("please enter a whole number")
+whole_number = int(input())
+
+#using the modulor operator
+if whole_number % 2 == 0:
+  print("your number {} is an even number". format(whole_number))
+else:
+  print("your number {} is an odd number". format(whole_number))  
