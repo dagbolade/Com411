1ae15917ca72e4972fbd95d46a10ed9cab81da23
repl.it user <replituser@example.com>
2020-11-05 10:31:53 +0000

    Updated main file

--- basics/repetition/while_loop/simple.py
+++ basics/repetition/while_loop/simple.py
@@ -1,9 +1,11 @@
-print("How many cables should i remove")
-user = int(input().lower())
+def run():
 
-cables = 0
-# to display number of counts
-while cables < user:
-  #display a message in the while loop
-  print("Removed cable")
-  cables = cables + 1
+  print("How many cables should i remove")
+  user = int(input().lower())
+
+  cables = 0
+  # to display number of counts
+  while cables < user:
+    #display a message in the while loop
+    print("Removed cable")
+    cables = cables + 1
