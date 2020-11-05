bb053e4941c5702c03af22e28f71055bf3de951d
repl.it user <replituser@example.com>
2020-11-05 16:46:27 +0000

    Updated main file

---
+++ basics/functions/simple_functions.py
@@ -0,0 +1,11 @@
+# create a program that listens for sounds in the cave.
+
+def listen():
+
+  print("What type of sound did you hear?")
+  user = input()
+  print("That was a loud {}!". format(user))
+
+  #call the function
+def run():
+  listen()
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import basics.repetition.while_loop.sum_100 as sum_100
 import basics.repetition.for_loop.simple as simplee
 import basics.repetition.for_loop.count_down as countd
 import basics.repetition.for_loop.characters as characters
+import basics.functions.simple_functions as functions
 
 
 print("To which program do you want to run")
@@ -17,7 +18,8 @@ programs=[
   "5. sum_100.py",
   "6. simple.py",
   "7. count_down.py",
-  "8. character.py"
+  "8. character.py",
+  "9. simple_functions.py"
 ]
 print(programs)
 user=int(input())
@@ -37,4 +39,6 @@ elif user == 6:
 elif user == 7:
   countd.run() 
 elif user == 8:
-  characters.run()                                           
+  characters.run()                                         
+elif user == 9:
+  functions.run()                                               
