1b0874d52c69662c0bec9b673d564a4df0254dd7
repl.it user <replituser@example.com>
2020-11-30 12:44:52 +0000

    updated the shift sine

--- visual/animations/shift_sine.py
+++ visual/animations/shift_sine.py
@@ -7,15 +7,16 @@ fig, ax = plt.subplots()
 def animate(frame):
   ax.cla()
 
-  ax.set_xlim(0, 720)
+  ax.set_xlim(0, 2*np.pi)
   ax.set_ylim(-1, 1)
-  x_values = np.arange(0, 720)
-  y_values =  np.sin((x_values + frame) *np.pi/180)
+  x = np.arange(0, (2 * np.pi), 0.01)
+  y = np.sin(x + (frame / 50))
+  ax.plot(x, y)
+
   
-  ax.plot(x_values, y_values, 'r')
 
 def run():
-  some_animation = animation.FuncAnimation(fig, animate, frames = 720, interval = 5)
+  some_animation = animation.FuncAnimation(fig, animate, frames = 720, interval = 100)
   plt.show()
 
 run()  
